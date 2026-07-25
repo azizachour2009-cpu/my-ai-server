bb0a7a3c57b64fa3df9f25b88a44b8f5bce73a9c
azizachour2009-cpu <azizachour2009@gmail.com>
2026-07-25 09:15:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+
   res.write("AI Server is working!");
   res.end();
 });
