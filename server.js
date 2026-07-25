004112d66c957f0cde3dab6e55ebbf47423e437c
azizachour2009-cpu <azizachour2009@gmail.com>
2026-07-25 09:03:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("AI Server is running");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.write("AI Server is working!");
+  res.end();
+});
+
+server.listen(3000, () => {
+  console.log("Server started");
+});
