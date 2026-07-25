cdc6b222faf330717fb2f60deb915a2d0e0de63c
azizachour2009-cpu <azizachour2009@gmail.com>
2026-07-25 09:18:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
 
-  res.write("AI Server is working!");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  const query = url.parse(req.url, true).query;
+
+  let message = query.msg || "ما كتبت حتى شيء";
+
+  res.write("إنت قلت: " + message);
   res.end();
+
 });
 
 server.listen(3000, () => {
